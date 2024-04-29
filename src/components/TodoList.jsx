f60a798f5db92c946685a3abd7986809a1665246
SAMYESSAM30 <samyessam2000@gmail.com>
2024-04-29 09:46:48 +0300

    add

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
-
+import { v4 as uuidv4 } from "uuid";
+import Todo from "./Todo";
+uuidv4();
 const TodoList = () => {
+  const [todoValue, setTodoValue] = useState([]);
+  const createTodo = (todo) => {
+    setTodoValue([
+      ...todoValue,
+      { id: uuidv4(), task: todo, isEditing: false },
+    ]);
+  };
   return (
-    <div className="container bg-blue-950 mt-20 p-8 rounded-md ">
-      <Form />
+    <div className="container bg-gray-700 mt-20 p-8 rounded-md ">
+      <Form createTodo={createTodo} />
+      {todoValue.map((todo, idx) => (
+        <Todo key={idx} task={todo} />
+      ))}
     </div>
   );
 };
