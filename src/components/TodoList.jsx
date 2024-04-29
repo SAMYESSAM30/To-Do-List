c18b348fe8319e1d7d8009dfa385e97e060859f0
SAMYESSAM30 <samyessam2000@gmail.com>
2024-04-29 10:17:23 +0300

    add delete

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -11,11 +11,26 @@ const TodoList = () => {
       { id: uuidv4(), task: todo, isEditing: false },
     ]);
   };
+  const deleteTodo = (id) => {
+    setTodoValue(todoValue.filter((todo) => todo.id !== id));
+  };
+  const editTodo = (id) => {
+    setTodoValue(
+      todoValue.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
   return (
     <div className="container bg-gray-700 mt-20 p-8 rounded-md ">
       <Form createTodo={createTodo} />
       {todoValue.map((todo, idx) => (
-        <Todo key={idx} task={todo} />
+        <Todo
+          key={idx}
+          task={todo}
+          deleteTodo={deleteTodo}
+          editTodo={editTodo}
+        />
       ))}
     </div>
   );
